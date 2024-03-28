49e9a0da3ca580fc46dc3893c007c2b39792441c
vijayAV55 <vijayram964932@gmail.com>
2024-03-28 17:14:52 +0530

    add

--- Javascript/JS7_DOM/CRUD_3/student.js
+++ Javascript/JS7_DOM/CRUD_3/student.js
@@ -12,32 +12,30 @@ function addStudent(){
         website: "",
         language: []
     };
-    for(a in formData){
-        if(a !== "pursuing" && a !== "language"){
-            formData[a] = document.getElementById(a).value
-        }else if(a === "pursuing"){
-            var allRadioBtns = document.querySelectorAll('input[type="radio"]');
-            for(i=0;i<allRadioBtns.length;i++){
-                if(allRadioBtns[i].checked === true){
-                    formData[a] = allRadioBtns[i].value
-                }
-            }
-        }else if(a === "language"){
-            var allCheckBoxs = document.querySelectorAll('input[type="checkbox"]');
-            for(i=0;i<allCheckBoxs.length;i++){
-                if(allCheckBoxs[i].checked === true){
-                    formData[a].push(allCheckBoxs[i].value)
-                }
-            }
+    // for(a in formData){
+    //     if(a !== "pursuing" && a !== "language"){
+    //         formData[a] = document.getElementById(a).value
+    //     }else if(a === "pursuing"){
+    //         var allRadioBtns = document.querySelectorAll('input[type="radio"]');
+    //         for(i=0;i<allRadioBtns.length;i++){
+    //             if(allRadioBtns[i].checked === true){
+    //                 formData[a] = allRadioBtns[i].value
+    //             }
+    //         }
+    //     }else if(a === "language"){
+    //         var allCheckBoxs = document.querySelectorAll('input[type="checkbox"]');
+    //         for(i=0;i<allCheckBoxs.length;i++){
+    //             if(allCheckBoxs[i].checked === true){
+    //                 formData[a].push(allCheckBoxs[i].value)
+    //             }
+    //         }
 
-        }
-    }
+    //     }
+    // }
 
     students.push(formData)
-
     displayUsers()
 }
-
 function displayUsers(){
     document.querySelector("tbody").innerHTML = "";
     for(i=0;i<students.length;i++){

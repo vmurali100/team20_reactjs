276d74d34650f0c60c1d525f81b3478106fc21cb
vijayAV55 <vijayram964932@gmail.com>
2024-04-20 14:57:53 +0530

    add

---
+++ youtube _projects/app1.js
@@ -0,0 +1,31 @@
+// select inital count
+let count = 0;
+// select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const style = e.currentTarget.classList;
+        if (style.contains('decrease')) {
+            count--;
+        }
+        else if (style.contains('increase')) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        if (count > 0) {
+            value.style.color = " green ";
+        }
+        if (count < 0) {
+            value.style.color = " red ";
+        }
+        if (count === 0) {
+            value.style.color = " #222 ";
+        }
+        value.textContent = count;
+    });
+
+});

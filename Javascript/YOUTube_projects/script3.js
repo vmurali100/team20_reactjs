814fee3f465ac38053d041dba1b3627d5cb249f0
vijayAV55 <vijayram964932@gmail.com>
2024-04-28 14:04:19 +0530

    add

--- Javascript/YOUTube_projects/script3.js
+++ Javascript/YOUTube_projects/script3.js
@@ -37,7 +37,7 @@ const questions = [
     }
 ];
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-button");
+const answerButton = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;

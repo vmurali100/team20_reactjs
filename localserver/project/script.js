c5ca5969e0b9921bff0ee838fca3d2c57b5c59e6
himadeep <matavalamhimadeepreddy@gmail.com>
2024-05-01 12:48:16 +0530

    added

---
+++ localserver/project/script.js
@@ -0,0 +1,65 @@
+
+const questions = [
+    {
+        questions: "which is the largest animal in the world?",
+        answers: [
+            { text: "shark", correct: false },
+            { text: "Blue whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+    },
+    {
+        questions: "which is the best smallest country in the world?",
+        answers: [
+            { text: "Vatican city", correct: true },
+            { text: "Bhutan", correct: false },
+            { text: "Nepal", correct: false },
+            { text: "shri Lanka", correct: false },
+        ]
+    },
+    {
+        questions: "which is the largest desert in the world?",
+        answers: [
+            { text: "kalahari", correct: false },
+            { text: "Gobi", correct: false },
+            { text: "sahara", correct: false },
+            { text: "Antarctica", correct: true },
+        ]
+    },
+    {
+        questions: "which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },
+            { text: "Arctic", correct: false },
+            { text: "Africa", correct: false },
+        ]
+    }
+];
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
+    question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendchild(button);
+    });
+}
+startQuiz();

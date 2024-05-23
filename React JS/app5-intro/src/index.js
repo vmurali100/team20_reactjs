36564624f88b68b5d5fcf9453c1e0fa421ed5573
mnethaji838 <mnethaji838@gmail.com>
2024-05-23 11:05:46 +0530

    add

--- React JS/app5-intro/src/index.js
+++ React JS/app5-intro/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Sample from "./Sample"
@@ -9,7 +8,7 @@ import Component from "./Sample1"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Component />
+    <App />
   </React.StrictMode>
 );
 

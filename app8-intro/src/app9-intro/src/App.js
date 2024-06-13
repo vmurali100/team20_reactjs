9acd7ee38b97ee3350d4697bc8eb128d6e053998
himadeep <matavalamhimadeepreddy@gmail.com>
2024-06-13 20:50:17 +0530

    added

---
+++ app8-intro/src/app9-intro/src/App.js
@@ -0,0 +1,19 @@
+import { Component } from 'react'
+import { Hima } from './practise/Hima'
+import { Reddy } from './practise/Reddy'
+import { Sample } from './Hooks/Sample1'
+
+export class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>himadeep</h1>
+      {/* <Hima/> */}
+      <Sample/>
+     
+     
+      
+        </div>
+    )
+  }
+}

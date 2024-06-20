dd140a0842b035fe39637bdda9ebb291cc52a894
mnethaji838 <mnethaji838@gmail.com>
2024-06-20 16:12:33 +0530

    add

--- React JS/Bootstrape/app10-intro/src/App.js
+++ React JS/Bootstrape/app10-intro/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Nethaji from './Bootstrape-Component/Nethaji';
+import Sai from './Bootstrape-component/Sai';
+import VijayDinesh from './Bootstrape-component/VijayDinesh';
 
 function App() {
   return (
     <div >
-     
+      {/* <Nethaji/> */}
+      <Sai/>
+      <VijayDinesh/>
     </div>
   );
 }
